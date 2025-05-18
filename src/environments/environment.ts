91c5f170aaab9ded953ba5c08319c1a3e88f5eb8
Cristian Garcia <cristiangarcianastar21@gmail.com>
2025-05-18 16:50:38 -0500

    Eliminacion informacion sensible

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
- production: false,
-  supabaseUrl: 'https://sziwdjwdwilobvvlkyro.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6aXdkandkd2lsb2J2dmxreXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTcxODYsImV4cCI6MjA2MzE3MzE4Nn0.zxR-tK6GuIUVP_X_tf0Gfn4OaKUn8zR5J_1lSwYKntI'
-};
